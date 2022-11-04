049d41c99f0b2edbc7690ab1da1b3366aaa7bf7b
zengchanghuan <zengchanghuan@gmail.com>
2022-11-04 16:33:47 +0800

    add linked list project

---
+++ linkedList/linkedList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  linkedList
+//
+//  Created by 曾长欢 on 2022/11/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
