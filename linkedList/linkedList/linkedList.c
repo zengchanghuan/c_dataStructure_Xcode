9edc1f157acf40689750e0cd17a0a2c64c09b28e
zengchanghuan <zengchanghuan@gmail.com>
2022-11-04 16:35:07 +0800

    add linked list file

---
+++ linkedList/linkedList/linkedList.c
@@ -0,0 +1,8 @@
+//
+//  linkedList.c
+//  linkedList
+//
+//  Created by 曾长欢 on 2022/11/4.
+//
+
+#include "linkedList.h"
---
+++ linkedList/linkedList/linkedList.h
@@ -0,0 +1,13 @@
+//
+//  linkedList.h
+//  linkedList
+//
+//  Created by 曾长欢 on 2022/11/4.
+//
+
+#ifndef linkedList_h
+#define linkedList_h
+
+#include <stdio.h>
+
+#endif /* linkedList_h */
