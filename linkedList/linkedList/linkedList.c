81d813bbda8a713a014abb55fc96e39464f87a78
zengchanghuan <zengchanghuan@gmail.com>
2022-11-04 16:37:13 +0800

    add linked list api

--- linkedList/linkedList/linkedList.c
+++ linkedList/linkedList/linkedList.c
@@ -6,3 +6,20 @@
 //
 
 #include "linkedList.h"
+#include <stdlib.h>     // 提供 malloc、realloc、free、exit 原型
+#include <string.h>     // 提供 strstr 原型
+
+void initList(LinkList head);
+
+bool insertList(LinkList head,Element element,int index);
+
+bool deleteList(LinkList head,int index);
+
+Element *getList(LinkList head,int index);
+
+int findList(LinkList head,Element element);
+
+int length(LinkList list);
+
+void printList(LinkList head);
+
--- linkedList/linkedList/linkedList.h
+++ linkedList/linkedList/linkedList.h
@@ -9,5 +9,30 @@
 #define linkedList_h
 
 #include <stdio.h>
+#include <stdbool.h>
 
+typedef int Element;
+
+struct ListNode{
+    Element element;
+    struct ListNode *next;
+};
+
+//别名
+typedef struct ListNode *Node;
+typedef struct ListNode *LinkList;
+
+void initList(LinkList head);
+
+bool insertList(LinkList head,Element element,int index);
+
+bool deleteList(LinkList head,int index);
+
+Element *getList(LinkList head,int index);
+
+int findList(LinkList head,Element element);
+
+int length(LinkList list);
+
+void printList(LinkList head);
 #endif /* linkedList_h */
