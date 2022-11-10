fd040dc642b15dbe8c47f2c572efdda37ae5836a
zengchanghuan <zengchanghuan@gmail.com>
2022-11-10 11:22:56 +0800

    按值删除等方法实现

--- doubleCircularLinkedList/main.c
+++ doubleCircularLinkedList/main.c
@@ -7,9 +7,19 @@ int main() {
         insertBack(&mylist,i+1);
         printList(&mylist);
     }
-//    DuLNode *node = findIndex(&mylist,4);
+
 
     printf("insertIndex %d", insertIndex(&mylist,5,453));
+    printf("insertIndex %d", insertIndex(&mylist,9,453));
+
+    printList(&mylist);
+//    DuLNode *node = findIndex(&mylist,5);
+
+//    deleteFront(&mylist);
+//    delete(&mylist,node);
+
+    deleteAllEqualValue(&mylist,453);
+
     printList(&mylist);
     return 0;
 }
